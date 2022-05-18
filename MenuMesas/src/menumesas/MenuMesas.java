3ac296555a42a645e2efd3a78c8d5e299b3e9644
Kevin Torralbo Garcia <56842901+Sirkevin22@users.noreply.github.com>
2022-05-19 00:59:10 +0200

    Update MenuMesas.java
    Últimos comentarios

--- MenuMesas/src/menumesas/MenuMesas.java
+++ MenuMesas/src/menumesas/MenuMesas.java
@@ -6,12 +6,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- *
  * @author Reiner
  * @author Kevin
- */
-public class MenuMesas {
+ **/
 
+public class MenuMesas {
+    
+    //MOSTRAR EL MENU POR PANTALLA =  Kevin TG
     public static int menu() {
         Scanner T = new Scanner(System.in);
         int n = 0;
@@ -31,36 +32,48 @@ public class MenuMesas {
         }
         return n;
     }
-
+    
+    //FUNCIONES DEL MENU MESAS = Kevin TG
     public static void main(String[] args) {
-        File fichero = new File("C:/Users/alumne/Desktop/Proyecto M03/Proyecto-M03---Gestio-de-Fitxers/taulas.txt");
+        
+        //Creamos el fichero para guardar la informacion de las mesas
+        File fichero = new File("C:/Users/alumne/Desktop/Proyecto M03/Proyecto-M03---Gestio-de-Fitxers/taulas.txt");        
+        //variables
         int opcio;
+        //Funcion donde tenemos el menu
         opcio = MenuMesas.menu();
+        
+        //Las funciones del menu mesas
         do {
             switch (opcio) {
+                //Case 1 Leer Tabla
                 case 1:
                     LeerTabla();
                     break;
+                //Case 2 Dar de alta
                 case 2:
                     EscribirTabla(fichero);
                     break;
+                //Case 3 Modificar las mesas
                 case 3:
-
                     editarMesa(fichero);
-
                     break;
-
+                //Case 4 Borramos las mesas
                 case 4:
                     borrarMesa(fichero);
                     break;
+                //Case 5 Salir del menu
                 case 5:
                     opcio = 5;
                     break;
             }
+            //Funcion donde tenemos el menu
             opcio = MenuMesas.menu();
+        //Funcion para salir del menu
         } while (opcio != 5);
     }
-
+    
+    //FUNCION QUE LEER TABLA = Kevin TG
     public static void LeerTabla() {
 
         File fichero = new File("C:/Users/alumne/Desktop/Proyecto M03/Proyecto-M03---Gestio-de-Fitxers/taulas.txt");
@@ -78,32 +91,39 @@ public class MenuMesas {
             System.out.println("Ha ocurrido un error al abrir/leer el fichero");
         }
     }
-
+    
+    //FUNCION DE PEDIR INFORMACION DE LAS MESAS = Reiner MR
     public static String nuevaMesa() {
+        
         Scanner lector = new Scanner(System.in);
-        System.out.println("Introduce la id: ");
+        
+        System.out.print("Introduce la id: ");
         String id = lector.next();
-        System.out.println("Descripcion mesa: ");
+        System.out.print("Descripcion mesa: ");
         String descripcion = lector.next();
-        System.out.println("Numero de sillas: ");
+        System.out.print("Numero de sillas: ");
         String numeroSillas = lector.next();
-        System.out.println("¿Tiene silla para bebes? ");
+        System.out.print("¿Tiene silla para bebes? ");
         String bb = lector.next();
-        System.out.println("Sillas de adultos: ");
+        System.out.print("Sillas de adultos: ");
         String sillasAdultos = lector.next();
-        System.out.println("¿Tiene Ventilador? ");
+        System.out.print("¿Tiene Ventilador? ");
         String ventilador = lector.next();
-        System.out.println("¿Esta en el jardin? ");
+        System.out.print("¿Esta en el jardin? ");
         String jardin = lector.next();
 
         String nuevaMesa = id + ";" + descripcion + ";" + numeroSillas ";" + bb + ";" + sillasAdultos + ";" + ventilador + ";" + jardin;
         return nuevaMesa;
     }
-
+    
+    //FUNCION DONDE EDITAMOS EL FICHERO LAS MESAS = Reiner MR
     public static void editarMesa(File fichero) {
+        
         Scanner lector = new Scanner(System.in);
+        
         String lineacreada, lineanueva;
-        System.out.println("Introduce que id quieres editar: ");
+        
+        System.out.print ("Introduce que id quieres editar: ");
         lineacreada = lector.nextLine();
         //Array para guardar todas las li­neas leÃ­das del fichero
         ArrayList<String> lineas = new ArrayList<>();
@@ -138,11 +158,15 @@ public class MenuMesas {
             System.out.println("Ha ocurrido un error al abrir/sobreescribir el fichero");
         }
     }
-
+    
+    //FUNCION DONDE BORRAMOS LAS MESAS = Reiner MR , Kevin TG
     private static void borrarMesa(File fichero) {
+        
         Scanner lector = new Scanner(System.in);
+        
         String lineaeliminada;
-        System.out.println("Introduce que id quieres eliminar: ");
+        
+        System.out.print("Introduce que id quieres eliminar: ");
         lineaeliminada = lector.nextLine();
 
         // Array para guardar todas las li­neas lei­das del fichero
@@ -176,7 +200,8 @@ public class MenuMesas {
             System.out.println("Ha ocurrido un error al abrir/sobreescribir el fichero");
         }
     }
-
+    
+    //FUNCION DONDE ESCRIBE LA TABLA = Reiner MR
     private static void EscribirTabla(File fichero) {
         String nuevaLinea = "\n" + nuevaMesa();
 
