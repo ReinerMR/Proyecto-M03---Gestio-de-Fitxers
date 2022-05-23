08e75648118b163f4f8accb70c90f9386aca00a4
Kevin Torralbo Garcia <56842901+Sirkevin22@users.noreply.github.com>
2022-05-23 20:18:14 +0200

    Actualizar Tablas

--- Proyecto M03/Proyecto-M03---Gestio-de-Fitxers/MenuMesas/src/menumesas/MenuMesas.java
+++ Proyecto M03/Proyecto-M03---Gestio-de-Fitxers/MenuMesas/src/menumesas/MenuMesas.java
@@ -1,85 +1,36 @@
 package menumesas;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
  * @author Reiner
  * @author Kevin
  **/
-
 public class MenuMesas {
     
-    //MOSTRAR EL MENU POR PANTALLA =  Kevin TG
-    public static int menu() {
-        Scanner T = new Scanner(System.in);
-        int n = 0;
-        while (n < 1 || n > 5) {
+    //ARRAY DE LOS USUARIOS = Kevin TG
+    public static Usuarios[] Usuarios;
 
-            System.out.println(">------------------------<");
-            System.out.println("--------MENU MESAS--------");
-            System.out.println(">------------------------<");
-            System.out.println("1 - LISTAR MESAS");
-            System.out.println("2 - DAR DE ALTA MESA");
-            System.out.println("3 - MODIFICAR MESA");
-            System.out.println("4 - BORRAR MESA");
-            System.out.println("5 - SALIR");
-            System.out.println(">------------------------<");
-            System.out.print("INTRODUCE LA OPCION: ");
-            n = T.nextInt();
-        }
-        return n;
-    }
-    
-    //FUNCIONES DEL MENU MESAS = Kevin TG
     public static void main(String[] args) {
-        
-        //Creamos el fichero para guardar la informacion de las mesas
-        File fichero = new File("C:/Users/alumne/Desktop/Proyecto M03/Proyecto-M03---Gestio-de-Fitxers/taulas.txt");        
-        //variables
-        int opcio;
-        //Funcion donde tenemos el menu
-        opcio = MenuMesas.menu();
-        
-        //Las funciones del menu mesas
-        do {
-            switch (opcio) {
-                //Case 1 Leer Tabla
-                case 1:
-                    LeerTabla();
-                    break;
-                //Case 2 Dar de alta
-                case 2:
-                    EscribirTabla(fichero);
-                    break;
-                //Case 3 Modificar las mesas
-                case 3:
-                    editarMesa(fichero);
-                    break;
-                //Case 4 Borramos las mesas
-                case 4:
-                    borrarMesa(fichero);
-                    break;
-                //Case 5 Salir del menu
-                case 5:
-                    opcio = 5;
-                    break;
-            }
-            //Funcion donde tenemos el menu
-            opcio = MenuMesas.menu();
-        //Funcion para salir del menu
-        } while (opcio != 5);
+        GenerarUsuario();
+        menuUsuarios();
     }
-    
+
     //FUNCION QUE LEER TABLA = Kevin TG
     public static void LeerTabla() {
 
-        File fichero = new File("C:/Users/alumne/Desktop/Proyecto M03/Proyecto-M03---Gestio-de-Fitxers/taulas.txt");
+        File fichero = new File("C:\\Users\\alumne\\Desktop\\Proyecto M03\\Proyecto-M03---Gestio-de-Fitxers\\taulas.txt");
 
         try {
-            // Codificación ISO-8859-1 (ANSI) o UTF-8 dependiendo de cómo esté creado el fichero de texto
+            // CodificaciÃ³n ISO-8859-1 (ANSI) o UTF-8 dependiendo de cÃ³mo estÃ© creado el fichero de texto
             Scanner lectorFichero = new Scanner(fichero, "ISO-8859-1");
 
             while (lectorFichero.hasNext()) {
@@ -88,44 +39,44 @@ public class MenuMesas {
 
             lectorFichero.close();
         } catch (Exception e) {
-            System.out.println("Ha ocurrido un error al abrir/leer el fichero");
+            System.out.println("ERROR AL LEER EL FICHERO");
         }
     }
-    
+
     //FUNCION DE PEDIR INFORMACION DE LAS MESAS = Reiner MR
     public static String nuevaMesa() {
-        
+
         Scanner lector = new Scanner(System.in);
-        
+
         System.out.print("Introduce la id: ");
         String id = lector.next();
         System.out.print("Descripcion mesa: ");
         String descripcion = lector.next();
         System.out.print("Numero de sillas: ");
         String numeroSillas = lector.next();
-        System.out.print("¿Tiene silla para bebes? ");
+        System.out.print("Â¿Tiene silla para bebes? ");
         String bb = lector.next();
         System.out.print("Sillas de adultos: ");
         String sillasAdultos = lector.next();
-        System.out.print("¿Tiene Ventilador? ");
+        System.out.print("Â¿Tiene Ventilador? ");
         String ventilador = lector.next();
-        System.out.print("¿Esta en el jardin? ");
+        System.out.print("Â¿Esta en el jardin? ");
         String jardin = lector.next();
 
         String nuevaMesa = id + ";" + descripcion + ";" + numeroSillas + ";" + bb + ";" + sillasAdultos + ";" + ventilador + ";" + jardin;
         return nuevaMesa;
     }
-    
+
     //FUNCION DONDE EDITAMOS EL FICHERO LAS MESAS = Reiner MR
     public static void editarMesa(File fichero) {
-        
+
         Scanner lector = new Scanner(System.in);
-        
+
         String lineacreada, lineanueva;
-        
-        System.out.print ("Introduce que id quieres editar: ");
+
+        System.out.print("Introduce que id quieres editar: ");
         lineacreada = lector.nextLine();
-        //Array para guardar todas las li­neas leÃ­das del fichero
+        
         ArrayList<String> lineas = new ArrayList<>();
 
         // Abrimos el fichero de texto para leerlo en memoria
@@ -137,7 +88,7 @@ public class MenuMesas {
 
             lectorFichero.close();
         } catch (Exception e) {
-            System.out.println("Ha ocurrido un error al abrir/leer el fichero");
+            System.out.println("ERROR AL EDITAR EL FICHERO");
         }
 
         // Abrimos el fichero de texto para sobreescribirlo
@@ -145,7 +96,7 @@ public class MenuMesas {
             FileWriter writer = new FileWriter(fichero);
 
             for (String linea : lineas) {
-                if (lineacreada.equals(linea.substring(0, 2))) {
+                if (lineacreada.equals(linea.substring(0, 3))) {
                     lineanueva = nuevaMesa();
                     writer.write(lineanueva + "\n");
                 } else {
@@ -158,14 +109,14 @@ public class MenuMesas {
             System.out.println("Ha ocurrido un error al abrir/sobreescribir el fichero");
         }
     }
-    
+
     //FUNCION DONDE BORRAMOS LAS MESAS = Reiner MR , Kevin TG
     private static void borrarMesa(File fichero) {
-        
+
         Scanner lector = new Scanner(System.in);
-        
+
         String lineaeliminada;
-        
+
         System.out.print("Introduce que id quieres eliminar: ");
         lineaeliminada = lector.nextLine();
 
@@ -182,7 +133,7 @@ public class MenuMesas {
 
             lectorFichero.close();
         } catch (Exception e) {
-            System.out.println("Ha ocurrido un error al abrir/leer el fichero");
+            System.out.println("ERROR AL BORRAR MESA");
         }
 
         // Abrimos el fichero de texto para sobreescribirlo
@@ -200,7 +151,7 @@ public class MenuMesas {
             System.out.println("Ha ocurrido un error al abrir/sobreescribir el fichero");
         }
     }
-    
+
     //FUNCION DONDE ESCRIBE LA TABLA = Reiner MR
     private static void EscribirTabla(File fichero) {
         String nuevaLinea = "\n" + nuevaMesa();
@@ -211,7 +162,244 @@ public class MenuMesas {
             writer.close();
 
         } catch (Exception e) {
-            System.out.println("Ha ocurrido un error al crear/escribir en el fichero");
+            System.out.println("ERROR AL ESCRIBIR");
         }
     }
+
+    //FUNCION DE MOSTRAR EL MENU USUARIOS = Reiner MR
+    private static void menuUsuarios() {
+        Scanner M = new Scanner(System.in);
+        int u = 0;
+        boolean salir = false;
+
+        while (!salir) {
+
+            System.out.println(">---------------------------<");
+            System.out.println("---------MENU ADMIN----------");
+            System.out.println(">---------------------------<");
+            System.out.println("1 - ADMINISTRADOR ");
+            System.out.println("2 - CAMAMARERO ");
+            System.out.println("3 - SALIR");
+            System.out.println(">---------------------------<");
+
+            try {
+
+                System.out.print("INTRODUCE LA OPCION: ");
+                u = M.nextInt();
+
+                //LAS FUNCIONES DE MENU ADMIN
+                switch (u) {
+                    //Case 1 Menu Admin
+                    case 1:
+                        menuAdmin();
+                        break;
+                    //Case 2 Menu Cambrer
+                    case 2:
+                        menuCambrer();
+                        break;
+                    //Case 3 Salir
+                    case 3:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("INTRODUCE DEL 1 AL 3");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("INTRODUCE LA OPCION: ");
+                M.nextInt();
+            }
+        }
+    }
+
+    //FUNCION MOSTRAR EL MENU CAMARERO POR PANTALLA =  Kevin TG, Reiner MR
+    private static void menuCambrer() {
+        Scanner M = new Scanner(System.in);
+        int c = 0;
+        boolean salir = false;
+
+        while (!salir) {
+
+            System.out.println(">---------------------------<");
+            System.out.println("---------MENU MESAS----------");
+            System.out.println(">---------------------------<");
+            System.out.println("1 - LISTAR MESAS");
+            System.out.println("2 - DAR DE ALTA MESA");
+            System.out.println("3 - MODIFICAR MESA");
+            System.out.println("4 - BORRAR MESA");
+            System.out.println("5 - SALIR");
+            System.out.println(">---------------------------<");
+
+            try {
+
+                System.out.print("INTRODUCE LA OPCION: ");
+                c = M.nextInt();
+
+                //Creamos el fichero para guardar la informacion de las mesas
+                File fichero = new File("C:\\Users\\alumne\\Desktop\\Proyecto M03\\Proyecto-M03---Gestio-de-Fitxers\\taulas.txt");
+
+                //Las funciones del menu mesas
+                switch (c) {
+                    //Case 1 LEER TABLA
+                    case 1:
+                        LeerTabla();
+                        break;
+                    //Case 2 DAR DE ALTA
+                    case 2:
+                        EscribirTabla(fichero);
+                        break;
+                    //Case 3 MODIFICAR LA MESA
+                    case 3:
+                        editarMesa(fichero);
+                        break;
+                    //Case 4 BORRAR LA MESA
+                    case 4:
+                        borrarMesa(fichero);
+                        break;
+                    //Case 5 VOLVER AL MENU USUARIOS
+                    case 5:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("INTRODUCE DEL 1 AL 5");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("INTRODUCE LA OPCION: ");
+                M.nextInt();
+            }
+        }
+    }
+
+    //FUNCION MOSTRAR EL MENU ADMIN POR PANTALLA = Reiner MR
+    private static void menuAdmin() {
+        Scanner M = new Scanner(System.in);
+        int a = 0;
+        boolean salir = false;
+
+        while (!salir) {
+
+            System.out.println(">---------------------------<");
+            System.out.println("--------MENU USUARIOS--------");
+            System.out.println(">---------------------------<");
+            System.out.println("1 - ALTA USUARIO");
+            System.out.println("2 - LISTAR USUARIOS");
+            System.out.println("3 - MODIFICAR USUARIOS");
+            System.out.println("4 - BORRAR USUARIO");
+            System.out.println("5 - SALIR");
+            System.out.println(">---------------------------<");
+
+            try {
+
+                System.out.print("INTRODUCE LA OPCION: ");
+                a = M.nextInt();
+
+                //Creamos el fichero para guardar la informacion de las mesas
+                File fichero = new File("C:\\Users\\alumne\\Desktop\\Proyecto M03\\Proyecto-M03---Gestio-de-Fitxers\\users.dat");
+
+                //Las funciones del menu mesas
+                switch (a) {
+                    //Case 1 DAR DE ALTA AL USUARIO COMO CAMBRER
+                    case 1:
+                        CrearUsuario();
+                        break;
+                    //Case 2 LISTAR USUARIOS
+                    case 2:
+                        MostrarUsuario();
+                        break;
+                    //Case 3 MODIFICAR USUARIOS
+                    case 3:
+
+                        break;
+                    //Case 4 BORRAR USUARIOS
+                    case 4:
+
+                        break;
+                    //Case 5 VOLVER AL MENU USUARIOS
+                    case 5:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("INTRODUCE DEL 1 AL 5");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("INTRODUCE LA OPCION: ");
+                M.nextInt();
+            }
+        }
+    }
+    
+    //FUNCION DONDE GENERA EL USUARIO PRINCIPAL = Kevin TG
+    private static void GenerarUsuario() {
+
+        try {
+            ObjectOutputStream fichero = new ObjectOutputStream(new FileOutputStream("C:\\Users\\alumne\\Desktop\\Proyecto M03\\Proyecto-M03---Gestio-de-Fitxers\\users.dat"));
+            Usuarios[] Usuarios = new Usuarios[10];
+            Usuarios[0] = new Usuarios();
+            Usuarios[0].rol = "Administrador";
+            Usuarios[0].usuario = "admin";
+            Usuarios[0].contraseña = "admin";
+            fichero.writeObject(Usuarios);
+            fichero.close();
+        } catch (Exception e) {
+            System.out.println("ERROR AL GENERAR USUARIO");
+        }
+    }
+    
+    //FUNCION DONDE CREA EL USUARIO = Kevin TG
+    private static void CrearUsuario() {
+
+        Scanner CU = new Scanner(System.in);
+        System.out.print("ROL: ");
+        String rol = CU.nextLine();
+        System.out.print("USUARIO: ");
+        String usuario = CU.nextLine();
+        System.out.print("CONTRASEÑA: ");
+        String contraseña = CU.nextLine();
+        int contadorUsuarios = 0;
+        boolean comprobar = true;
+        try {
+            ObjectOutputStream fichero = new ObjectOutputStream(new FileOutputStream("C:\\Users\\alumne\\Desktop\\Proyecto M03\\Proyecto-M03---Gestio-de-Fitxers\\users.dat"));          
+            
+            Usuarios[] Usuarios = new Usuarios[10];
+            
+            while (comprobar != false) {
+                contadorUsuarios++;
+                if (Usuarios[contadorUsuarios] == null) {
+                    Usuarios[contadorUsuarios] = new Usuarios();
+                    Usuarios[contadorUsuarios].rol = rol;
+                    Usuarios[contadorUsuarios].usuario = usuario;
+                    Usuarios[contadorUsuarios].contraseña = contraseña;
+                    comprobar = false;
+                }
+
+            }
+
+            fichero.writeObject(Usuarios);
+            fichero.close();
+        } catch (Exception e) {
+            System.out.println("ERROR AL CREAR USUARIO");
+        }
+    }
+    
+    //FUNCION MOSTRAR LOS USUARIOS POR PANTALLA = Kevin TG
+    private static void MostrarUsuario() {
+
+        try {
+            ObjectInputStream fichero = new ObjectInputStream(new FileInputStream("C:\\Users\\alumne\\Desktop\\Proyecto M03\\Proyecto-M03---Gestio-de-Fitxers\\users.dat"));
+            Usuarios[] Usuarios = (Usuarios[]) fichero.readObject();
+            for (Usuarios usuario : Usuarios) {
+                if (usuario != null) {
+                    System.out.println("Rol: " + usuario.rol);
+                    System.out.println("Usuario: " + usuario.usuario);
+                    System.out.println("Contraseña: " + usuario.contraseña);
+                    System.out.println(">---------------------------<");
+                }
+            }
+            fichero.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("ERROR AL MOSTRAR EL FICHERO");
+        }
+
+    }
+
 }
